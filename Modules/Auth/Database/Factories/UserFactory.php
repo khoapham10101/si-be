b0664970e2d8a4c74ddc856a3190ad947a5472e8
Khoa Pham <phamdangkhoa1010@gmail.com>
2023-09-19 12:33:29 +0700

    Test user authentication

---
+++ Modules/Auth/Database/Factories/UserFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Auth\Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Modules\User\Entities\User;
+
+class UserFactory extends Factory
+{
+    protected $model = User::class;
+
+    public function definition()
+    {
+        return [
+            'first_name' => $this->faker->name,
+            'last_name' => $this->faker->name,
+            'id_card' => $this->faker->unique()->numerify('##########'),
+            'user_status_id' => 0,
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => bcrypt('password'), // Adjust the password as needed
+        ];
+    }
+}
---
+++ Modules/Auth/Tests/Feature/AuthTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Modules\Auth\Tests;
+
+use Tests\TestCase;
+use Modules\Auth\Database\Factories\UserFactory;
+/**
+ * Tests for AuthController.
+ * php artisan test Modules/Auth/Tests/Feature/AuthTest.php
+ */
+class AuthTest extends TestCase
+{
+    private $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = UserFactory::new()->create();
+    }
+
+    public function test_user_can_login_with_valid_credentials()
+    {
+        $response = $this->post('/api/v1/login', [
+            'email' => $this->user->email,
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'data' => [
+                'access_token',
+                'user',
+            ],
+        ]);
+    }
+
+    public function test_user_cannot_login_with_invalid_credentials()
+    {
+        $response = $this->post('/api/v1/login', [
+            'email' => $this->user->email,
+            'password' => 'invalidpassword',
+        ]);
+
+        $response->assertStatus(401);
+    }
+
+    public function test_user_can_logout()
+    {
+        $token = $this->user->createToken('testToken')->plainTextToken;
+
+        $response = $this->withHeader('Authorization', "Bearer $token")->post('/api/v1/logout');
+
+        $response->assertStatus(200);
+        $response->assertJson(['message' => 'OK']);
+    }
+
+    public function tearDown(): void
+    {
+        // Clear user tokens
+        $this->user->tokens()->delete();
+
+        parent::tearDown();
+    }
+}
