2185816d6976b49e2819ae945ede78274c08e367
Khoa Pham <phamdangkhoa1010@gmail.com>
2023-09-25 16:23:45 +0700

    Add unittests for Product

---
+++ Modules/Product/Tests/Feature/ProductControllerTest.php
@@ -0,0 +1,298 @@
+<?php
+
+namespace Modules\Product\Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Modules\Brand\Database\Factories\BrandFactory;
+use Modules\Permission\Database\Factories\PermissionFactory;
+use Modules\Role\Database\Factories\RoleFactory;
+use Tests\TestCase;
+use Modules\User\Database\Factories\UserFactory;
+
+class ProductControllerTest extends TestCase
+{
+    use RefreshDatabase;
+    private $user;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = UserFactory::new()->create();
+    }
+
+    protected function setUpCommonData()
+    {
+        // Create a new role
+        $role = RoleFactory::new()->create();
+
+        // Assign role to user
+        $this->user->roles()->attach([$role->id]);
+        $this->assertTrue($this->user->roles->contains($role));
+
+        // Create permissions
+        $permissionList = PermissionFactory::new()->create([
+            'name' => 'List Products',
+            'action' => 'admin.products.index',
+            'module_name' => 'Products'
+        ]);
+
+        $permissionCreate = PermissionFactory::new()->create([
+            'name' => 'Create Product',
+            'action' => 'admin.products.create',
+            'module_name' => 'Products'
+        ]);
+
+        $permissionUpdate = PermissionFactory::new()->create([
+            'name' => 'Update Product',
+            'action' => 'admin.products.edit',
+            'module_name' => 'Products'
+        ]);
+
+        $permissionDelete = PermissionFactory::new()->create([
+            'name' => 'Delete Product',
+            'action' => 'admin.products.destroy',
+            'module_name' => 'Products'
+        ]);
+
+        // Assign role to permissions
+        $role->permissions()->attach([
+            $permissionList->id,
+            $permissionCreate->id,
+            $permissionUpdate->id,
+            $permissionDelete->id
+        ]);
+    }
+
+    public function test_user_can_get_list_products()
+    {
+        $this->actingAs($this->user);
+        $this->setUpCommonData();
+
+        // Craete brand
+        $brand = BrandFactory::new()->create([
+            'name' => 'Brand 1'
+        ]);
+
+        // Add product
+        $response = $this->postJson('/api/v1/products/create', [
+            'name' => 'Product 1',
+            'brand_id' => $brand->id,
+            'sku' => '1234MVC',
+            'description' => '',
+            'warranty_information' => '',
+            'quantity' => '123',
+            'price' => '123'
+        ]);
+
+        $response = $this->post('/api/v1/products');
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'data' => [
+                [
+                    'id',
+                    'name',
+                    'brand_id',
+                    'brand' => []
+                ]
+            ],
+        ]);
+    }
+
+    public function test_user_cannot_get_list_products_without_permission()
+    {
+        $this->actingAs($this->user);
+
+        $response = $this->post('/api/v1/products');
+        $response->assertStatus(403);
+        $response->assertJson([
+            'success' => false,
+            'code' => 403,
+            'message' => 'Forbidden'
+        ]);
+    }
+
+    public function test_user_can_add_product()
+    {
+        $this->actingAs($this->user);
+        $this->setUpCommonData();
+
+        // Craete brand
+        $brand = BrandFactory::new()->create([
+            'name' => 'Brand 1'
+        ]);
+
+        // Add new product
+        $response = $this->postJson('/api/v1/products/create', [
+            'name' => 'Product 1',
+            'brand_id' => $brand->id,
+            'sku' => '1234MVC',
+            'description' => '',
+            'warranty_information' => '',
+            'quantity' => '123',
+            'price' => '123'
+        ]);
+        $response->assertStatus(201);
+        $response->assertJsonFragment(['name' => 'Product 1']);
+    }
+
+    public function test_user_cannot_add_product_without_validate_pass()
+    {
+        $this->actingAs($this->user);
+        $this->setUpCommonData();
+
+        // Craete brand
+        $brand = BrandFactory::new()->create([
+            'name' => 'Brand 1'
+        ]);
+
+        // Add new product
+        $response = $this->postJson('/api/v1/products/create', [
+            'name' => '',
+            'brand_id' => $brand->id,
+            'sku' => '1234MVC',
+            'description' => '',
+            'warranty_information' => '',
+            'quantity' => '123',
+            'price' => '123'
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors(['name']);
+    }
+
+    public function test_user_cannot_add_user_without_permission()
+    {
+        $this->actingAs($this->user);
+
+        // Craete brand
+        $brand = BrandFactory::new()->create([
+            'name' => 'Brand 1'
+        ]);
+
+        // Add new product
+        $response = $this->postJson('/api/v1/products/create', [
+            'name' => 'Product 1',
+            'brand_id' => $brand->id,
+            'sku' => '1234MVC',
+            'description' => '',
+            'warranty_information' => '',
+            'quantity' => '123',
+            'price' => '123'
+        ]);
+        $response->assertStatus(403);
+        $response->assertJson([
+            'success' => false,
+            'code' => 403,
+            'message' => 'Forbidden'
+        ]);
+    }
+
+    public function test_user_can_update_product()
+    {
+        $this->actingAs($this->user);
+        $this->setUpCommonData();
+
+        // Craete brand
+        $brand = BrandFactory::new()->create([
+            'name' => 'Brand 1'
+        ]);
+
+        // Add new product
+        $response = $this->postJson('/api/v1/products/create', [
+            'name' => 'Product 1',
+            'brand_id' => $brand->id,
+            'sku' => '1234MVC',
+            'description' => '',
+            'warranty_information' => '',
+            'quantity' => '123',
+            'price' => '123'
+        ]);
+        $response->assertStatus(201);
+        $response->assertJsonFragment(['name' => 'Product 1']);
+
+        // Get the ID of the newly created product
+        $newProductId = $response->json('id');
+        // Edit product
+        $response = $this->postJson("/api/v1/products/update/{$newProductId}", [
+            'name' => 'Product 1 edit',
+            'brand_id' => $brand->id,
+            'sku' => '1234MVC',
+            'description' => '',
+            'warranty_information' => '',
+            'quantity' => '123',
+            'price' => '123'
+        ]);
+        $response->assertStatus(200);
+        $response->assertJsonFragment(['name' => 'Product 1 edit']);
+    }
+
+    public function test_user_can_delete_product()
+    {
+        $this->actingAs($this->user);
+        $this->setUpCommonData();
+
+        // Craete brand
+        $brand = BrandFactory::new()->create([
+            'name' => 'Brand 1'
+        ]);
+
+        // Add new product
+        $response = $this->postJson('/api/v1/products/create', [
+            'name' => 'Product 1',
+            'brand_id' => $brand->id,
+            'sku' => '1234MVC',
+            'description' => '',
+            'warranty_information' => '',
+            'quantity' => '123',
+            'price' => '123'
+        ]);
+        $response->assertStatus(201);
+        $response->assertJsonFragment(['name' => 'Product 1']);
+
+        // Get the ID of the newly created product
+        $newProductId = $response->json('id');
+        // Remove product
+        $remove = $this->delete("/api/v1/products/delete/{$newProductId}");
+        $remove->assertStatus(204);
+    }
+
+    public function test_user_cannot_delete_product_when_does_not_exist()
+    {
+        $this->actingAs($this->user);
+        $this->setUpCommonData();
+
+        // Craete brand
+        $brand = BrandFactory::new()->create([
+            'name' => 'Brand 1'
+        ]);
+
+        // Add new product
+        $response = $this->postJson('/api/v1/products/create', [
+            'name' => 'Product 1',
+            'brand_id' => $brand->id,
+            'sku' => '1234MVC',
+            'description' => '',
+            'warranty_information' => '',
+            'quantity' => '123',
+            'price' => '123'
+        ]);
+        $response->assertStatus(201);
+        $response->assertJsonFragment(['name' => 'Product 1']);
+        // Get the ID of the newly created product
+        $newProductId = 123456789;
+        // Remove product
+        $remove = $this->delete("/api/v1/products/delete/{$newProductId}");
+        $remove->assertStatus(404);
+        $remove->assertJson([
+            'success' => false,
+            'message' => 'Product 123456789 not found'
+        ]);
+    }
+
+    public function tearDown(): void
+    {
+        $this->user->tokens()->delete();
+        parent::tearDown();
+    }
+}
